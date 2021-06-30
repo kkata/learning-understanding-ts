c1c000e905583955fbd355d492ddd15f83d8aa6c
kkata <14849535+kkata@users.noreply.github.com>
2021-06-30 23:04:36 +0900

    29. never 型

--- app.ts
+++ app.ts
@@ -7,3 +7,12 @@ userInput = "Max";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+// 戻り値を絶対返さない関数
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+const result = generateError("エラーが発生しました", 500);
+//出力されない
+console.log(result);
