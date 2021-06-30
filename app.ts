9ba770982b3efa34ffccea22f8b511fd91fadc8d
kkata <14849535+kkata@users.noreply.github.com>
2021-06-30 22:36:14 +0900

    26. function 型

--- app.ts
+++ app.ts
@@ -8,4 +8,13 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+// let combineValues: Function;
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = 5;
+// combineValues = printResult;
+
+console.log(combineValues(8, 8));
+
 printResult(add(5, 12));
