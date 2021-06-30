8ffdc2e0d5f716ec8266c0816570e6feacb87fa6
kkata <14849535+kkata@users.noreply.github.com>
2021-06-30 22:59:06 +0900

    28. unknown 型

--- app.ts
+++ app.ts
@@ -1,27 +1,9 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
-
-// void型はreturn値がない場合使う
-// 以下の場合は本来不要(学習目的で記述)
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-addAndHandle(10, 20, (result) => console.log(result));
-
-// let combineValues: Function;
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-// combineValues = 5;
-// combineValues = printResult;
-
-console.log(combineValues(8, 8));
-
-printResult(add(5, 12));
