6cec5d293602763fe3b4a5909a9f18bc27c9ecd3
kkata <14849535+kkata@users.noreply.github.com>
2021-07-04 18:46:24 +0900

    61. private and public modifier

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 class Department {
   name: string;
+  private employees: string[] = [];
+
   constructor(n: string) {
     this.name = n;
   }
@@ -8,12 +10,26 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("accounting");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+// This is bad.
+// accounting.employees[2] = "Mike";
+
+accounting.printEmployeeInformation();
+
 accounting.describe();
 
-const accountingCopy = { name: "Dummy", describe: accounting.describe };
-
-accountingCopy.describe();
+// const accountingCopy = { name: "Dummy", describe: accounting.describe };
+// accountingCopy.describe();
