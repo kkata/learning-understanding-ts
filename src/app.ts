6b9874dc86ef3db1036079ddfe623cf3ab2bfb15
kkata <14849535+kkata@users.noreply.github.com>
2021-07-04 18:36:13 +0900

    60. constructor and this

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("start!");
+class Department {
+  name: string;
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  // thisを型付けする
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("accounting");
+
+accounting.describe();
+
+const accountingCopy = { name: "Dummy", describe: accounting.describe };
+
+accountingCopy.describe();
