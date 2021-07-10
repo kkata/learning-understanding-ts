2832cbc5558601258b29b304c7b3de58c3f7aa61
kkata <14849535+kkata@users.noreply.github.com>
2021-07-10 17:47:05 +0900

    82. 交差型

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+// interface でも同じことができる
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+// interface ElevatedEmployee extends Admin, Employee {}
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+// number型になる
+type Universal = Combinable & Numeric;
