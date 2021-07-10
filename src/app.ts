bd811a4e6c4e99e672e63c417d0f4f31c40f07d0
kkata <14849535+kkata@users.noreply.github.com>
2021-07-10 20:50:42 +0900

    95. Generics に制約を追加する

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,11 @@ promise.then((data) => {
   // data.sprit(" ");
 });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
 const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 
 console.log(mergedObj.name);
-console.log(mergedObj.hobbies[0]);
+console.log(mergedObj);
