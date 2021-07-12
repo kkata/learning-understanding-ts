f0e4aa95f7cd59cdd3897ec00e9927842f0d2ca9
kkata <14849535+kkata@users.noreply.github.com>
2021-07-12 17:27:45 +0900

    98. Generic Class

--- src/app.ts
+++ src/app.ts
@@ -42,3 +42,35 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: "" }, "name");
+
+// Genericクラス
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1); // 要素が見つからない時、indexOfは-1を返す
+  }
+
+  getItem() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Data1");
+textStorage.addItem("Data2");
+textStorage.removeItem("Data1");
+console.log(textStorage.getItem());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const obj = { name: "Max" };
+// objStorage.addItem(obj);
+// objStorage.addItem({ name: "Manu" });
+// objStorage.removeItem(obj);
+// console.log(objStorage.getItem());
