d4770961bedec17016d782614f0952f4e9d64829
kkata <14849535+kkata@users.noreply.github.com>
2021-07-15 09:47:23 +0900

    109. アクセサとパラメータのデコレータ

--- src/app.ts
+++ src/app.ts
@@ -43,11 +43,37 @@ function Log(target: any, propertyName: string | Symbol) {
   console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor デコレータ");
+  console.log("target", target);
+  console.log("name", name);
+  console.log("descriptor", descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method デコレータ");
+  console.log("target", target);
+  console.log("name", name);
+  console.log("descriptor", descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter デコレータ");
+  console.log("target", target);
+  console.log("name", name);
+  console.log("position", position);
+}
+
 class Product {
   @Log
   title: string;
   private _price: number;
 
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -60,7 +86,9 @@ class Product {
     this.title = t;
     this._price = p;
   }
-  getPriceWithTax(tax: number) {
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this.price * (1 + tax);
   }
 }
