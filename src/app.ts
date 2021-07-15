6ef28c388e88a9c770fba7f2b31b8a7e3854ffe3
kkata <14849535+kkata@users.noreply.github.com>
2021-07-15 17:58:03 +0900

    115-116. デコレータによるバリデーション - 完成

--- src/app.ts
+++ src/app.ts
@@ -129,12 +129,55 @@ button.addEventListener("click", p.showMessage);
 
 // ---
 // デコレータによるバリデーション実装
+interface ValidatorConfig {
+  // インデックス型の入れ子
+  [prop: string]: {
+    [validatableProp: string]: string[]; // ['required', 'positive']
+  };
+}
 
-function Required() {}
+const registeredValidators: ValidatorConfig = {};
 
-function PositiveNumber() {}
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "required",
+    ],
+  };
+}
 
-function validate(obj: object) {}
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "positive",
+    ],
+  };
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case "required":
+          isValid = isValid && !!obj[prop];
+          break;
+        case "positive":
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
 class Course {
   @Required
   title: string;
