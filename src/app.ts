a0e822813bce88e71751cb03b3aee57fb1db9f9e
kkata <14849535+kkata@users.noreply.github.com>
2021-07-15 10:36:40 +0900

    113. 例：Autobind デコレータの作成

--- src/app.ts
+++ src/app.ts
@@ -98,3 +98,31 @@ class Product {
     return this.price * (1 + tax);
   }
 }
+
+// thisをバインドするMethodデコレータ
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "クリックしました";
+
+  @AutoBind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
