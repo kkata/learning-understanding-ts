82e2659113fbb423d2b949d696e3434ce1ff3c28
kkata <14849535+kkata@users.noreply.github.com>
2021-07-21 16:43:51 +0900

    129. Enum によるプロジェクトのフィルタリング

--- src/app.ts
+++ src/app.ts
@@ -121,7 +121,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -133,6 +139,7 @@ class ProjectList {
     const listEl = document.getElementById(
       `${this.type}-list`
     )! as HTMLUListElement;
+    listEl.innerHTML = "";
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement("li");
       listItem.textContent = prjItem.title;
