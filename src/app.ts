d489315da54893770d3fc71d9b6045215d0791a2
kkata <14849535+kkata@users.noreply.github.com>
2021-07-22 23:25:35 +0900

    135. ドロップ可能な場所

--- src/app.ts
+++ src/app.ts
@@ -180,7 +180,8 @@ class ProjectItem
 
   @AutoBind2
   dragStartHandler(event: DragEvent) {
-    console.log(event);
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
   }
 
   dragEndHandler(_: DragEvent) {
@@ -214,12 +215,17 @@ class ProjectList
   }
 
   @AutoBind2
-  dragOverHandler(_: DragEvent) {
-    const listEl = this.element.querySelector("ul")!;
-    listEl.classList.add("droppable");
+  dragOverHandler(event: DragEvent) {
+    if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+      event.preventDefault();
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.add("droppable");
+    }
   }
 
-  dropHandler(_: DragEvent) {}
+  dropHandler(event: DragEvent) {
+    console.log(event.dataTransfer!.getData("text/plain"));
+  }
 
   @AutoBind2
   dragLeaveHandler(_: DragEvent) {
