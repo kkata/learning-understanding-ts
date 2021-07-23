0a0139226f0dd39e4835ff0f6f98164c08ee62d0
kkata <14849535+kkata@users.noreply.github.com>
2021-07-23 19:46:23 +0900

    145. ESモジュールの利用

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,13 +1,7 @@
-namespace App {
-  export function AutoBind2(
-    _: any,
-    _2: string,
-    descriptor: PropertyDescriptor
-  ) {
-    return {
-      get() {
-        return descriptor.value.bind(this);
-      },
-    };
-  }
+export function AutoBind2(_: any, _2: string, descriptor: PropertyDescriptor) {
+  return {
+    get() {
+      return descriptor.value.bind(this);
+    },
+  };
 }
