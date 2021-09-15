d3faf841a2d69bd1ffe62b0128efc1b0f5eb8dfb
kkata <14849535+kkata@users.noreply.github.com>
2021-09-15 21:33:41 +0900

    Generic 関数の作成

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
-const names: Array<string> = []; // string[]
+// const names: Array<string> = []; // string[]
 
-// promiseは他の型も返すので、それをGeneric型で定義する
-// 戻り値の型がわかることで、TypeScriptのサポートが利くようになる
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Done!");
-  });
-});
+// // promiseは他の型も返すので、それをGeneric型で定義する
+// // 戻り値の型がわかることで、TypeScriptのサポートが利くようになる
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Done!");
+//   });
+// });
 
-promise.then((data) => {
-  data.split("");
-});
+// promise.then((data) => {
+//   data.split("");
+// });
+
+// TとUは関数を呼び出したときに推定される
+function merge<T, U>(obgA: T, objB: U) {
+  return Object.assign(obgA, objB);
+}
+
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+
+console.log(mergedObj.name);
