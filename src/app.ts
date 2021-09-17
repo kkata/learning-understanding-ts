4ff93db507897bdbe30039d9496d79dbf9a42c87
kkata <14849535+kkata@users.noreply.github.com>
2021-09-17 18:41:52 +0900

    複数のデコレータの追加

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
+// デコレータ関数の作成はJavaScriptの順序どおり、上から下に行われる
+// デコレータ関数の実行は下から上に向かって実行される
 function Logger(logString: string) {
+  console.log("LOGGER ファクトリ");
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -6,7 +9,9 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log("TEMPLATE ファクトリ");
   return function (constructor: any) {
+    console.log("テンプレートを表示");
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
@@ -17,6 +22,7 @@ function WithTemplate(template: string, hookId: string) {
 }
 
 // @Logger("ログ出力中 - Person")
+@Logger("ログ出力中")
 @WithTemplate("<h1>Personオブジェクト</h1>", "app")
 class Person {
   name = "Max";
