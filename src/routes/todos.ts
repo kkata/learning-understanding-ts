95e18f98f7006fdf2e6084b37dd9f8b0ea7d3106
kkata <14849535+kkata@users.noreply.github.com>
2022-02-02 11:57:54 +0900

    コントローラの作成 リクエストボディの取得

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 
+import { createTodo } from "../controllers/todos";
+
 const router = Router();
 
-router.post("/");
+router.post("/", createTodo);
+
 router.get("/");
+
 router.patch("/:id");
+
 router.delete("/:id");
 
 export default router;
